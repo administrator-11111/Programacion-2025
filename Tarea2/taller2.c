7f83f585158967f3910c5a95ee7ae76fc7b7346f
administrator-11111 <cuentasamp2022@gmail.com>
2025-05-19 16:21:36 -0400

    Terminado SIN LA FECHA REGISTRADA :C

--- Tarea2/taller2.c
+++ Tarea2/taller2.c
@@ -10,8 +10,44 @@
 #include <stdio.h>
 
 int main() {
+    int tiempo, dia, i = 0, paga = 0;
+    
+    printf("Ingrese el número correspondiente al día de la semana.\n1.Lunes\n2.Martes\n3.Miércoles\n4.Jueves\n5.Viernes\n6.Sábado\n7.Domingo\n");
+    while(i == 0) {
+        printf("Día: ");
+        scanf("%d", &dia);
+        if (dia > 0 && dia < 8) {
+            printf("Ahora ingrese el tiempo que estuvo estacionado(en minutos): ");
+            scanf("%d", &tiempo);
+            if (tiempo > 0) {
+                if (tiempo < 60) { //Si es una hora
+                    if (tiempo <= 10) { //si son 10 minutos
+                        paga = 0;   //Gratis
+                    } else {
+                        if (tiempo <= (10 + 15)) { // 10 minutos + siguientes 15 minutos
+                            paga += 3;  //Se aumenta $3
+                        }
+                    }
+                } else {
+                    paga += 10; //Primera hora
+                    if((tiempo/60) > 1) {   //Si el tiempo es +1 horas
+                        paga += 5*((tiempo-60)/60);
+                    }
+                }
+                if (dia == 7) {
+                    paga = paga*0.10;
+                }
+                printf("----------------------------------------------------");
+                printf("Horas/min\tTotal a pagar\n%d hrs, %d min\t$%d\n", tiempo/60, tiempo%60, paga);
+                i++;
+            } else {
+                printf("No se ingresó un tiempo válido.");
+            }
+        } else {
+            
+            printf("Por favor, ingrese un día válido.\n");
+        }
+    }
 
-    printf("\
-        Horas/min\tTotal a pagar\n");
     return 0;
 }
