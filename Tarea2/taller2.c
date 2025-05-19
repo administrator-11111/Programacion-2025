806fc9ee5d5710809b8397b85b5e72444325ed2a
return <123399398+administrator-11111@users.noreply.github.com>
2025-05-19 15:06:56 -0400

    taller 2 init

---
+++ Tarea2/taller2.c
@@ -0,0 +1,17 @@
+/*Detalles:
+-Primeros 10 min gratis
+-Siguientes 15 min $3
+-Primera hora $10
+-Cada hora subsecuente $5
+-A partir de la primera hora se cobran horas completas
+-Si es Domingo se hace descuento de 10% sobre el monto total
+*Leer minutos y horas e impromor ciantp se pagara
+*/
+#include <stdio.h>
+
+int main() {
+
+    printf("\
+        Horas/min\tTotal a pagar\n");
+    return 0;
+}
